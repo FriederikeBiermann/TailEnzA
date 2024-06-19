e3a4f60adb60c058d2790f493fa03b51148da58e
FriederikeBiermann <friederike@biermann-erfurt.de>
2024-06-19 18:21:29 +0200

    Added finer grained metrics

--- tailenza/prediction/prediction_module_from_fasta.py
+++ tailenza/prediction/prediction_module_from_fasta.py
@@ -7,8 +7,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 from Bio import SeqIO, AlignIO
-from Bio.Seq import Seq
-from Bio.SeqRecord import SeqRecord
 from sklearn.base import BaseEstimator
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 import torch
@@ -113,10 +111,8 @@ def classifier_prediction(feature_matrix, classifier_path, mode):
         classifier.eval()
         classifier.to(device)
         with torch.no_grad():
-            if isinstance(feature_matrix, pd.DataFrame):
-                feature_matrix = feature_matrix.to_numpy()
             feature_matrix = torch.tensor(
-                feature_matrix, dtype=torch.float32
+                feature_matrix.to_numpy(), dtype=torch.float32
             ).to(device)
             logits = classifier(feature_matrix)
 
@@ -132,7 +128,7 @@ def classifier_prediction(feature_matrix, classifier_path, mode):
     return predicted_values, score_predicted_values
 
 
-def plot_confusion_matrix(cm, labels, title="Confusion Matrix"):
+def plot_confusion_matrix(cm, labels, title="Confusion Matrix", save_path=None):
     plt.figure(figsize=(10, 7))
     sns.heatmap(
         cm, annot=True, fmt="d", cmap="Blues", xticklabels=labels, yticklabels=labels
@@ -140,10 +136,11 @@ def plot_confusion_matrix(cm, labels, title="Confusion Matrix"):
     plt.title(title)
     plt.xlabel("Predicted")
     plt.ylabel("True")
-    plt.show()
+    if save_path:
+        plt.savefig(save_path)
 
 
-def plot_classification_report(cr, title="Classification Report"):
+def plot_classification_report(cr, title="Classification Report", save_path=None):
     lines = cr.split("\n")
     classes = []
     plot_data = []
@@ -163,7 +160,13 @@ def plot_classification_report(cr, title="Classification Report"):
     plt.title(title)
     plt.xlabel("Metrics")
     plt.ylabel("Classes")
-    plt.show()
+    if save_path:
+        plt.savefig(save_path)
+
+
+def save_classification_report(cr, save_path):
+    with open(save_path, "w") as f:
+        f.write(cr)
 
 
 def get_true_BGC_label_from_filename(filename, BGC_types):
@@ -175,7 +178,7 @@ def get_true_BGC_label_from_filename(filename, BGC_types):
 
 
 logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+    level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s"
 )
 package_dir = files("tailenza").joinpath("")
 logging.debug("Package directory: %s", package_dir)
@@ -184,12 +187,8 @@ directory_of_classifiers_BGC_type = "../classifiers/classifiers/BGC_type/"
 directory_of_classifiers_NP_affiliation = "../classifiers/classifiers/metabolism_type/"
 
 
-device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
-# Initialize lists for true and predicted labels
-true_BGC_labels = []
-predicted_BGC_labels = []
-true_metabolism_labels = []
-predicted_metabolism_labels = []
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 
 include_charge_features = True
 
@@ -198,137 +197,116 @@ include_charge_features = True
 def main():
     file_path_model = package_dir.joinpath("data", "esm1b_t33_650M_UR50S.pt")
     model, alphabet = esm.pretrained.load_model_and_alphabet_local(file_path_model)
-    model = model.eval().to(device)
+    model = model.eval()
     batch_converter = alphabet.get_batch_converter()
     for enzyme in enzymes:
+        # Initialize lists for true and predicted labels
+        true_BGC_labels = []
+        predicted_BGC_labels = []
+        true_metabolism_labels = []
+        predicted_metabolism_labels = []
         for BGC_type in BGC_types:
-            fasta_filename = f"validation_dataset/MIBiG_dataset/{enzyme}_MIBiG_{BGC_type}_without_too_long_too_short.fasta"
-            if not os.path.isfile(fasta_filename):
-                print("not_found")
-                continue
-            sequences = list(SeqIO.parse(fasta_filename, "fasta"))
-    
-            # Add the reference sequence
-            reference_seq = enzymes[enzyme]["reference_for_alignment"]
-            reference_record = SeqRecord(Seq(reference_seq), id="Reference")
-            batch_size = 100
-            # Split the sequences into batches of specified size
-            batches = [sequences[i:i + batch_size] for i in range(0, len(sequences), batch_size)]
+            fasta_filename = f"/validation_dataset/MIBiG_dataset/{enzyme}_MIBiG_{BGC_type}_without_too_long_too_short.fasta"
+            alignment = muscle_align_sequences(fasta_filename, enzyme, enzymes)
 
-            # Placeholder to store feature matrices
-            combined_feature_matrix = None
+            fragment_matrix = fragment_alignment(
+                alignment, enzymes[enzyme]["splitting_list"]
+            )
+            feature_matrix = featurize_fragments(
+                fragment_matrix, batch_converter, model, include_charge_features, device
+            )
 
-            for i, batch in enumerate(batches):
-                # Add the reference sequence to the current batch
-                batch.append(reference_record)
-
-                # Create a temporary FASTA file for the current batch
-                temp_fasta_filename = f"{enzyme}_batch_{i+1}.fasta"
-                with open(temp_fasta_filename, "w") as output_handle:
-                    SeqIO.write(batch, output_handle, "fasta")
-        
-                # Align the sequences in the batch
-                alignment = muscle_align_sequences(temp_fasta_filename, enzyme, enzymes)
-                print(f"Alignment for batch {i+1}:", alignment, enzymes[enzyme]["splitting_list"])
-        
-                # Fragment the alignment
-                fragment_matrix = fragment_alignment(
-                    alignment, enzymes[enzyme]["splitting_list"], True
-                )
-                print(f"Fragment matrix for batch {i+1}:", fragment_matrix)
-        
-                # Featurize the fragments
-                feature_matrix = featurize_fragments(
-                    fragment_matrix, batch_converter, model, include_charge_features, device
-                )
-
-                # Concatenate the feature matrix for the batch with the combined feature matrix
-                if combined_feature_matrix is None:
-                    combined_feature_matrix = feature_matrix
-                else:
-                    combined_feature_matrix = np.concatenate((combined_feature_matrix, feature_matrix), axis=0)
-        
-                # Remove the temporary FASTA file
-                os.remove(temp_fasta_filename)
-
-            feature_matrix = combined_feature_matrix
             predicted_metabolisms = {}
             scores_predicted_metabolism = {}
             predicted_BGC_types = {}
             scores_predicted_BGC_type = {}
             metabolism_classifier_path = os.path.join(
                 directory_of_classifiers_NP_affiliation,
-                f"{enzyme}{enzymes[enzyme]['classifier_metabolism']}",
+                enzymes[enzyme]["classifier_metabolism"],
             )
             BGC_type_classifier_path = os.path.join(
                 directory_of_classifiers_BGC_type,
-                f"{enzyme}{enzymes[enzyme]['classifier_BGC_type']}",
+                enzymes[enzyme]["classifier_BGC_type"],
             )
 
             (
                 predicted_metabolisms,
                 scores_predicted_metabolism,
             ) = classifier_prediction(
-                feature_matrix, metabolism_classifier_path, "metabolism"
+                feature_matrix, metabolism_classifier_path, device
             )
-            if enzyme == "ycao":
-                predicted_BGC_types = ["RiPP"] * len(combined_feature_matrix)
-                scores_predicted_BGC_type = [1] * len(combined_feature_matrix)
-            else:
-                (
-                    predicted_BGC_types,
-                    scores_predicted_BGC_type,
-                ) = classifier_prediction(feature_matrix, BGC_type_classifier_path, "BGC")
+            (
+                predicted_BGC_types,
+                scores_predicted_BGC_type,
+            ) = classifier_prediction(feature_matrix, BGC_type_classifier_path, device)
 
-            true_BGC_label = [BGC_type] * len(combined_feature_matrix)
+            true_BGC_label = [BGC_type] * len(fragment_matrix)
             true_BGC_labels.extend(true_BGC_label)
-            true_metabolism_label = ["secondary metabolism"] * len(combined_feature_matrix)
-            true_metabolism_labels.extend(true_metabolism_label)
+            true_metabolism_labels = ["secondary metabolism"] * len(fragment_matrix)
+            true_metabolism_labels.extend(true_metabolism_labels)
+
             predicted_BGC_labels.extend(predicted_BGC_types)
             predicted_metabolism_labels.extend(predicted_metabolisms)
 
-    # Calculate and print metrics for BGC type classification
-    print("BGC Type Classification Report:")
-    bgc_classification_report = classification_report(
-        true_BGC_labels, predicted_BGC_labels, labels=BGC_types
-    )
-    print(bgc_classification_report)
-    bgc_confusion_matrix = confusion_matrix(
-        true_BGC_labels, predicted_BGC_labels, labels=BGC_types
-    )
-    print("Confusion Matrix:\n", bgc_confusion_matrix)
+        # Calculate and print metrics for BGC type classification
+        print("BGC Type Classification Report:")
+        bgc_classification_report = classification_report(
+            true_BGC_labels, predicted_BGC_labels, labels=BGC_types
+        )
+        print(bgc_classification_report)
+        bgc_confusion_matrix = confusion_matrix(
+            true_BGC_labels, predicted_BGC_labels, labels=BGC_types
+        )
+        print("Confusion Matrix:\n", bgc_confusion_matrix)
 
-    # Calculate and print metrics for metabolism classification
-    print("Metabolism Classification Report:")
-    metabolism_classification_report = classification_report(
-        true_metabolism_labels,
-        predicted_metabolism_labels,
-        labels=["secondary metabolism"],
-    )
-    print(metabolism_classification_report)
-    metabolism_confusion_matrix = confusion_matrix(
-        true_metabolism_labels,
-        predicted_metabolism_labels,
-        labels=["secondary metabolism"],
-    )
-    print("Confusion Matrix:\n", metabolism_confusion_matrix)
+        # Calculate and print metrics for metabolism classification
+        print("Metabolism Classification Report:")
+        metabolism_classification_report = classification_report(
+            true_metabolism_labels,
+            predicted_metabolism_labels,
+            labels=["secondary metabolism"],
+        )
+        print(metabolism_classification_report)
+        metabolism_confusion_matrix = confusion_matrix(
+            true_metabolism_labels,
+            predicted_metabolism_labels,
+            labels=["secondary metabolism"],
+        )
+        print("Confusion Matrix:\n", metabolism_confusion_matrix)
 
-    # Plot confusion matrices
-    plot_confusion_matrix(
-        bgc_confusion_matrix, BGC_types, title="BGC Type Confusion Matrix"
-    )
-    plot_confusion_matrix(
-        metabolism_confusion_matrix,
-        ["secondary metabolism"],
-        title="Metabolism Confusion Matrix",
-    )
+        # Plot confusion matrices
+        cm_save_path = f"confusion_matrices/{enzyme}_BGC_type_confusion_matrix.png"
+        plot_confusion_matrix(
+            bgc_confusion_matrix,
+            BGC_types,
+            title="BGC Type Confusion Matrix",
+            save_path=cm_save_path,
+        )
+        cr_save_path = (
+            f"classification_reports/{enzyme}_BGC_type_classification_report.txt"
+        )
+        save_classification_report(bgc_classification_report, cr_save_path)
+        cm_save_path_metabolism = (
+            f"confusion_matrices/{enzyme}_metabolism_type_confusion_matrix.png"
+        )
 
-    # Plot classification reports
-    plot_classification_report(
-        bgc_classification_report, title="BGC Type Classification Report"
-    )
-    plot_classification_report(
-        metabolism_classification_report, title="Metabolism Classification Report"
-    )
-if __name__ == "__main__":
-    main()
+        plot_confusion_matrix(
+            metabolism_confusion_matrix,
+            ["secondary metabolism"],
+            title="Metabolism Confusion Matrix",
+            save_path=cm_save_path_metabolism,
+        )
+        cr_save_path_metabolism = (
+            f"classification_reports/{enzyme}_metabolism_type_classification_report.txt"
+        )
+        save_classification_report(
+            metabolism_classification_report, cr_save_path_metabolism
+        )
+
+        # Plot classification reports
+        plot_classification_report(
+            bgc_classification_report, title="BGC Type Classification Report"
+        )
+        plot_classification_report(
+            metabolism_classification_report, title="Metabolism Classification Report"
+        )
