7351c7c8c53a2f16d3aa1a8ad23550a74ea0c4e7
FriederikeBiermann <friederike@biermann-erfurt.de>
2024-06-20 08:49:05 +0200

    combined bug fixes

--- tailenza/prediction/prediction_module_from_fasta.py
+++ tailenza/prediction/prediction_module_from_fasta.py
@@ -7,6 +7,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 from Bio import SeqIO, AlignIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
 from sklearn.base import BaseEstimator
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 import torch
@@ -111,9 +113,11 @@ def classifier_prediction(feature_matrix, classifier_path, mode):
         classifier.eval()
         classifier.to(device)
         with torch.no_grad():
-            feature_matrix = torch.tensor(
-                feature_matrix.to_numpy(), dtype=torch.float32
-            ).to(device)
+            if isinstance(feature_matrix, pd.DataFrame):
+                feature_matrix = feature_matrix.to_numpy()
+            feature_matrix = torch.tensor(feature_matrix, dtype=torch.float32).to(
+                device
+            )
             logits = classifier(feature_matrix)
 
             predicted_values = torch.argmax(logits, dim=1).cpu().numpy()
@@ -187,7 +191,7 @@ directory_of_classifiers_BGC_type = "../classifiers/classifiers/BGC_type/"
 directory_of_classifiers_NP_affiliation = "../classifiers/classifiers/metabolism_type/"
 
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
 
 
 include_charge_features = True
@@ -197,7 +201,7 @@ include_charge_features = True
 def main():
     file_path_model = package_dir.joinpath("data", "esm1b_t33_650M_UR50S.pt")
     model, alphabet = esm.pretrained.load_model_and_alphabet_local(file_path_model)
-    model = model.eval()
+    model = model.eval().to(device)
     batch_converter = alphabet.get_batch_converter()
     for enzyme in enzymes:
         # Initialize lists for true and predicted labels
@@ -206,45 +210,105 @@ def main():
         true_metabolism_labels = []
         predicted_metabolism_labels = []
         for BGC_type in BGC_types:
-            fasta_filename = f"/validation_dataset/MIBiG_dataset/{enzyme}_MIBiG_{BGC_type}_without_too_long_too_short.fasta"
-            alignment = muscle_align_sequences(fasta_filename, enzyme, enzymes)
+            fasta_filename = f"validation_dataset/MIBiG_dataset/{enzyme}_MIBiG_{BGC_type}_without_too_long_too_short.fasta"
+            if not os.path.isfile(fasta_filename):
+                print("not_found")
+                continue
+            sequences = list(SeqIO.parse(fasta_filename, "fasta"))
 
-            fragment_matrix = fragment_alignment(
-                alignment, enzymes[enzyme]["splitting_list"]
-            )
-            feature_matrix = featurize_fragments(
-                fragment_matrix, batch_converter, model, include_charge_features, device
-            )
+            # Add the reference sequence
+            reference_seq = enzymes[enzyme]["reference_for_alignment"]
+            reference_record = SeqRecord(Seq(reference_seq), id="Reference")
+            batch_size = 100
+            # Split the sequences into batches of specified size
+            batches = [
+                sequences[i : i + batch_size]
+                for i in range(0, len(sequences), batch_size)
+            ]
 
+            # Placeholder to store feature matrices
+            combined_feature_matrix = None
+
+            for i, batch in enumerate(batches):
+                # Add the reference sequence to the current batch
+                batch.append(reference_record)
+
+                # Create a temporary FASTA file for the current batch
+                temp_fasta_filename = f"{enzyme}_batch_{i+1}.fasta"
+                with open(temp_fasta_filename, "w") as output_handle:
+                    SeqIO.write(batch, output_handle, "fasta")
+
+                # Align the sequences in the batch
+                alignment = muscle_align_sequences(temp_fasta_filename, enzyme, enzymes)
+                print(
+                    f"Alignment for batch {i+1}:",
+                    alignment,
+                    enzymes[enzyme]["splitting_list"],
+                )
+
+                # Fragment the alignment
+                fragment_matrix = fragment_alignment(
+                    alignment, enzymes[enzyme]["splitting_list"], True
+                )
+                print(f"Fragment matrix for batch {i+1}:", fragment_matrix)
+
+                # Featurize the fragments
+                feature_matrix = featurize_fragments(
+                    fragment_matrix,
+                    batch_converter,
+                    model,
+                    include_charge_features,
+                    device,
+                )
+
+                # Concatenate the feature matrix for the batch with the combined feature matrix
+                if combined_feature_matrix is None:
+                    combined_feature_matrix = feature_matrix
+                else:
+                    combined_feature_matrix = np.concatenate(
+                        (combined_feature_matrix, feature_matrix), axis=0
+                    )
+
+                # Remove the temporary FASTA file
+                os.remove(temp_fasta_filename)
+
+            feature_matrix = combined_feature_matrix
             predicted_metabolisms = {}
             scores_predicted_metabolism = {}
             predicted_BGC_types = {}
             scores_predicted_BGC_type = {}
             metabolism_classifier_path = os.path.join(
                 directory_of_classifiers_NP_affiliation,
-                enzymes[enzyme]["classifier_metabolism"],
+                f"{enzyme}{enzymes[enzyme]['classifier_metabolism']}",
             )
             BGC_type_classifier_path = os.path.join(
                 directory_of_classifiers_BGC_type,
-                enzymes[enzyme]["classifier_BGC_type"],
+                f"{enzyme}{enzymes[enzyme]['classifier_BGC_type']}",
             )
 
             (
                 predicted_metabolisms,
                 scores_predicted_metabolism,
             ) = classifier_prediction(
-                feature_matrix, metabolism_classifier_path, device
+                feature_matrix, metabolism_classifier_path, "metabolism"
             )
-            (
-                predicted_BGC_types,
-                scores_predicted_BGC_type,
-            ) = classifier_prediction(feature_matrix, BGC_type_classifier_path, device)
+            if enzyme == "ycao":
+                predicted_BGC_types = ["RiPP"] * len(combined_feature_matrix)
+                scores_predicted_BGC_type = [1] * len(combined_feature_matrix)
+            else:
+                (
+                    predicted_BGC_types,
+                    scores_predicted_BGC_type,
+                ) = classifier_prediction(
+                    feature_matrix, BGC_type_classifier_path, "BGC"
+                )
 
-            true_BGC_label = [BGC_type] * len(fragment_matrix)
+            true_BGC_label = [BGC_type] * len(combined_feature_matrix)
             true_BGC_labels.extend(true_BGC_label)
-            true_metabolism_labels = ["secondary metabolism"] * len(fragment_matrix)
-            true_metabolism_labels.extend(true_metabolism_labels)
-
+            true_metabolism_label = ["secondary metabolism"] * len(
+                combined_feature_matrix
+            )
+            true_metabolism_labels.extend(true_metabolism_label)
             predicted_BGC_labels.extend(predicted_BGC_types)
             predicted_metabolism_labels.extend(predicted_metabolisms)
 
