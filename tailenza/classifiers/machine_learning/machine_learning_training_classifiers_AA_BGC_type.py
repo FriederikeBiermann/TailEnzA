9f4e4f8aadbd6bd7a233f401337f2daf0a4c8afb
FriederikeBiermann <friederike@biermann-erfurt.de>
2024-07-02 16:44:29 +0200

    Tried to reduce overfitting

--- tailenza/classifiers/machine_learning/machine_learning_training_classifiers_AA_BGC_type.py
+++ tailenza/classifiers/machine_learning/machine_learning_training_classifiers_AA_BGC_type.py
@@ -14,6 +14,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 import torch.nn.functional as F
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
@@ -45,14 +46,16 @@ class BasicFFNN(nn.Module):
     def __init__(self, in_features: int, num_classes: int):
         super(BasicFFNN, self).__init__()
         self.fc1 = nn.Linear(in_features, 64)
-        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(0.2)
         self.fc2 = nn.Linear(64, num_classes)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
+        x = self.dropout(x)
         x = self.fc2(x)
         x = F.softmax(x, dim=1)
         return x
+
 
 
 # FFNN Model 2: Intermediate Feedforward Neural Network
@@ -60,14 +63,16 @@ class IntermediateFFNN(nn.Module):
     def __init__(self, in_features: int, num_classes: int):
         super(IntermediateFFNN, self).__init__()
         self.fc1 = nn.Linear(in_features, 128)
-        self.relu = nn.ReLU()
+        self.dropout1 = nn.Dropout(0.2)
         self.fc2 = nn.Linear(128, 64)
-        self.relu = nn.ReLU()
+        self.dropout2 = nn.Dropout(0.2)
         self.fc3 = nn.Linear(64, num_classes)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
+        x = self.dropout1(x)
         x = F.relu(self.fc2(x))
+        x = self.dropout2(x)
         x = self.fc3(x)
         x = F.softmax(x, dim=1)
         return x
@@ -78,20 +83,24 @@ class AdvancedFFNN(nn.Module):
     def __init__(self, in_features: int, num_classes: int):
         super(AdvancedFFNN, self).__init__()
         self.fc1 = nn.Linear(in_features, 256)
-        self.relu = nn.ReLU()
+        self.dropout1 = nn.Dropout(0.2)
         self.fc2 = nn.Linear(256, 128)
-        self.relu = nn.ReLU()
+        self.dropout2 = nn.Dropout(0.2)
         self.fc3 = nn.Linear(128, 64)
-        self.relu = nn.ReLU()
+        self.dropout3 = nn.Dropout(0.2)
         self.fc4 = nn.Linear(64, 32)
-        self.relu = nn.ReLU()
+        self.dropout4 = nn.Dropout(0.2)
         self.fc5 = nn.Linear(32, num_classes)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
+        x = self.dropout1(x)
         x = F.relu(self.fc2(x))
+        x = self.dropout2(x)
         x = F.relu(self.fc3(x))
+        x = self.dropout3(x)
         x = F.relu(self.fc4(x))
+        x = self.dropout4(x)
         x = self.fc5(x)
         x = F.softmax(x, dim=1)
         return x
@@ -101,91 +110,63 @@ class VeryAdvancedFFNN(nn.Module):
     def __init__(self, in_features: int, num_classes: int):
         super(VeryAdvancedFFNN, self).__init__()
         self.fc1 = nn.Linear(in_features, 512)
-        self.relu = nn.ReLU()
+        self.dropout1 = nn.Dropout(0.5)
         self.fc2 = nn.Linear(512, 256)
-        self.relu = nn.ReLU()
+        self.dropout2 = nn.Dropout(0.5)
         self.fc3 = nn.Linear(256, 128)
-        self.relu = nn.ReLU()
+        self.dropout3 = nn.Dropout(0.5)
         self.fc4 = nn.Linear(128, 64)
-        self.relu = nn.ReLU()
         self.fc5 = nn.Linear(64, 32)
-        self.relu = nn.ReLU()
         self.fc6 = nn.Linear(32, num_classes)
-
-        self.dropout = nn.Dropout(0.5)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
-        x = self.dropout(x)
+        x = self.dropout1(x)
         x = F.relu(self.fc2(x))
-        x = self.dropout(x)
+        x = self.dropout2(x)
         x = F.relu(self.fc3(x))
-        x = self.dropout(x)
+        x = self.dropout3(x)
         x = F.relu(self.fc4(x))
         x = F.relu(self.fc5(x))
         x = self.fc6(x)
         x = F.softmax(x, dim=1)
         return x
 
-
 # Define the Convolutional Neural Network (CNN)
 class CNN(nn.Module):
     def __init__(self, total_features: int, num_fragments: int, num_classes: int):
         super(CNN, self).__init__()
         self.num_fragments = num_fragments
+        self.features_per_fragment = (total_features - num_fragments - 1) // num_fragments
+        assert (total_features - num_fragments - 1) % num_fragments == 0, "Total features do not evenly divide into fragments"
 
-        # Calculate the features per fragment
-        self.features_per_fragment = (
-            total_features - num_fragments - 1
-        ) // num_fragments
-        assert (
-            total_features - num_fragments - 1
-        ) % num_fragments == 0, "Total features do not evenly divide into fragments"
-
-        self.conv1 = nn.Conv2d(
-            2, 32, kernel_size=3, stride=1, padding=1
-        )  # 2 channels: 1 for features, 1 for charges
+        self.conv1 = nn.Conv2d(2, 32, kernel_size=3, stride=1, padding=1)
         self.relu = nn.ReLU()
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.dropout = nn.Dropout(0.2)
 
-        # Calculate the correct size for the fully connected layer input
-        self.fc1_input_dim = (
-            64 * (self.num_fragments // 4) * (self.features_per_fragment // 4)
-        )
+        self.fc1_input_dim = 64 * (self.num_fragments // 4) * (self.features_per_fragment // 4)
         self.fc1 = nn.Linear(self.fc1_input_dim, 128)
         self.fc2 = nn.Linear(128, num_classes)
 
     def forward(self, x):
-        # Remove the last feature
         x = x[:, :-1]
+        charges = x[:, -self.num_fragments:]
+        main_features = x[:, :-self.num_fragments]
 
-        # Extract charge features (last num_fragments features)
-        charges = x[:, -self.num_fragments :]
-
-        # Extract main features (all but last num_fragments features)
-        main_features = x[:, : -self.num_fragments]
-
-        # Reshape main features to (batch_size, 1, num_fragments, features_per_fragment)
         batch_size = x.size(0)
-        main_features = main_features.view(
-            batch_size, 1, self.num_fragments, self.features_per_fragment
-        )
-
-        # Reshape charges to match the fragments shape (batch_size, 1, num_fragments, 1)
+        main_features = main_features.view(batch_size, 1, self.num_fragments, self.features_per_fragment)
         charges = charges.view(batch_size, 1, self.num_fragments, 1)
-
-        # Repeat charges to match the feature dimension (batch_size, 1, num_fragments, features_per_fragment)
         charges = charges.repeat(1, 1, 1, self.features_per_fragment)
-
-        # Combine main features and charges along the channel dimension
         x = torch.cat((main_features, charges), dim=1)
 
         x = self.pool(self.relu(self.conv1(x)))
         x = self.pool(self.relu(self.conv2(x)))
+        x = self.dropout(x)
 
         x = x.view(batch_size, -1)
-        x = self.relu(self.fc1(x))
+        x = F.relu(self.fc1(x))
         x = self.fc2(x)
         x = F.softmax(x, dim=1)
         return x
@@ -193,51 +174,36 @@ class CNN(nn.Module):
 
 # Define the Recurrent Neural Network (RNN)
 class RNN(nn.Module):
-    def __init__(
-        self, in_features: int, hidden_size: int, num_fragments: int, num_classes: int
-    ):
+    def __init__(self, in_features: int, hidden_size: int, num_fragments: int, num_classes: int):
         super(RNN, self).__init__()
         self.hidden_size = hidden_size
-
         self.num_fragments = num_fragments
-        self.features_per_fragment = (
-            in_features - num_fragments - 1
-        ) // num_fragments + 1
-        assert (
-            in_features - num_fragments - 1
-        ) % num_fragments == 0, "Total features do not evenly divide into fragments"
+        self.features_per_fragment = (in_features - num_fragments - 1) // num_fragments + 1
+        assert (in_features - num_fragments - 1) % num_fragments == 0, "Total features do not evenly divide into fragments"
+        
         self.rnn = nn.RNN(self.features_per_fragment, hidden_size, batch_first=True)
         self.fc = nn.Linear(hidden_size, num_classes)
+        self.dropout = nn.Dropout(0.2)
 
     def forward(self, x):
-        # Remove the last feature
         x = x[:, :-1]
+        charges = x[:, -self.num_fragments:]
+        main_features = x[:, :-self.num_fragments]
 
-        # Extract charge features (last num_fragments features)
-        charges = x[:, -self.num_fragments :]
-
-        # Extract main features (all but last num_fragments features)
-        main_features = x[:, : -self.num_fragments]
-
-        # Reshape main features to (batch_size, num_fragments, features_per_fragment)
         batch_size = x.size(0)
         seq_length = self.num_fragments
         input_size = main_features.size(1) // self.num_fragments
         main_features = main_features.view(batch_size, seq_length, input_size)
 
-        # Reshape charges to match the sequence shape (batch_size, num_fragments, 1)
         charges = charges.view(batch_size, seq_length, 1)
-
-        # Combine main features and charges along the last dimension
         x = torch.cat((main_features, charges), dim=2)
 
-        # Initialize hidden state
         h0 = torch.zeros(1, x.size(0), self.hidden_size).to(x.device)
-
         out, _ = self.rnn(x, h0)
         out = out[:, -1, :]
+        out = self.dropout(out)
         out = self.fc(out)
-        x = F.softmax(x, dim=1)
+        out = F.softmax(out, dim=1)
         return out
 
 
@@ -248,11 +214,11 @@ class LSTM(nn.Module):
         self.hidden_size = hidden_size
         self.lstm = nn.LSTM(in_features, hidden_size, batch_first=True)
         self.fc = nn.Linear(hidden_size, num_classes)
+        self.dropout = nn.Dropout(0.2)
 
     def forward(self, x):
-        # Ensure input has three dimensions
         if len(x.shape) == 2:
-            x = x.unsqueeze(1)  # Add a sequence length dimension
+            x = x.unsqueeze(1)
         elif len(x.shape) != 3:
             raise RuntimeError(f"Input must have 3 dimensions, got {x.shape}")
 
@@ -260,9 +226,11 @@ class LSTM(nn.Module):
         c0 = torch.zeros(1, x.size(0), self.hidden_size).to(x.device)
         out, _ = self.lstm(x, (h0, c0))
         out = out[:, -1, :]
+        out = self.dropout(out)
         out = self.fc(out)
         out = F.softmax(out, dim=1)
         return out
+
 
 
 
